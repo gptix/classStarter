4168a063ca5392e585d5de6bd2261304ffa99d08
Jud Taylor <jud.taylor@protonmail.com>
2020-02-16 19:05:47 -0500

    Added .gitignore

--- classstarter2.lisp
+++ classstarter2.lisp
@@ -1,3 +1,5 @@
+(in-package "COMMON-LISP-USER")
+
 (ql:quickload 'sqlite)
 
 (defpackage classstarter
@@ -9,42 +11,10 @@
 
 (setf *db* (sqlite:connect "/home/gt/test.sqlite3"))
 
-(setf *table-defs* (list "create table person 
- (id integer primary key, 
-  name not null, 
-  paypal_email not null,
-  other_email null,
-  phone null, 
-  type not_null)" 
+(load "table-def-strings.lisp")
 
-  "create table lesson_definition
-  (id integer primary key, 
-  name not null, 
-  hyperlink null,
-  description not null)" 
 
-  "create table scheduled_event
-  (id integer primary key, 
-  lesson_def_id not null,
-  date not null, 
-  venue not null,
-  min_rev not null,
-  max_students not null,
-  suggested_min not_null,
-  FOREIGN KEY(lesson_def_id) 
-  REFERENCES lesson_definition(id))"
 
- "CREATE TABLE bids
- (id integer PRIMARY KEY,
- person_id not null,
- date not null,
- scheduled_event_id not null,
- seats_desired not null,
- bid_per_seat not null,
- FOREIGN KEY(scheduled_event_id) REFERENCES scheduled_event(id),
- FOREIGN KEY(person_id) REFERENCES person(id))" )
-
-      
 (defun make-tables ()
   (loop :for str :in *table-defs*
 	:do (execute-non-query *db* str)))
---
+++ classstarter3.lisp
@@ -0,0 +1,47 @@
+#|
+https://github.com/fukamachi/mito/
+https://lispcookbook.github.io/cl-cookbook/databases.html
+|#
+
+(ql:quickload 'mito)
+
+(mito:connect-toplevel :sqlite3 :database-name "/home/gt/test.sqlite3")
+
+(mito:deftable person ()
+  ((name :col-type (:varchar 64))
+   (email :col-type (or (:varchar 128) :null))
+   (paypal-email :col-type (or (:varchar 128) :null))
+   (phone-number :col-type (:varchar 15) :null)
+   (role :col-type role))
+   ))
+
+(mito:deftable role ()
+  ((name :col-type (:varchar 64))
+   ))
+
+(mito:deftable event-class ()
+  ((name :col-type (:varchar 64))
+   (description  :col-type :bytea :null)
+   (materials  :col-type :bytea :null)
+   (prerequisites  :col-type :bytea :null)
+   (time-required-minutes :col-type :integer)
+))
+
+(mito:deftable venue ()
+  ((name :col-type (:varchar 64))
+   ))
+
+(mito:deftable occurence ()
+  ((venue  :col-type venue)
+   (date :col-type :integer)
+   (start :col-type :integer)
+   (stop :col-type :integer)
+   (attendee-max :col-type :integer)
+   (min fee :col-type :integer)
+   (min revenue :col-type :integer)))
+
+(mito:deftable bid ()
+  ((occurence :col-type occurence)
+   (attendee :col-type person)
+   (seat-count :col-type :integer)
+   (bid-per-seat :col-type :integer)))
